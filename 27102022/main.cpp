3ad4c9b63c32bf0c2c34f688f959f8ab4eab695f
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2022-10-27 17:25:43 +0200

    Add files via upload

---
+++ 27102022/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   /**  string znak;
+    int god;
+    cout << "Koliko imate godina: " ;
+    cin>>god;
+    if(god>0 && god<18)
+    {
+        cout << "Vi ste maloljetni."<<endl ;
+    }
+    else
+    {
+        cout << "Vi ste punoljetni"<<endl ;
+    }
+    cout << "Unesite ime i prezime: " ;
+    cin.ignore();
+    getline(cin,znak);
+    if(znak=="Mickey Mouse")
+    {
+        cout<<"I ja isto volim Disney!"<<endl;
+    }
+    else
+    {
+        cout<<"Unijeli ste dosadno ime i prezime"<<endl;
+
+     */
+
+    }
+
+    return 0;
+}
